b34d96dfbab437cf58ab142a0d3d1037524fbdb0
Michal8705 <Michal8705@gmail.com>
2020-07-01 20:37:00 +0200

    Modul 18.3

--- src/main/java/com/crud/tasks/domain/TrelloAttachmentsByType.java
+++ src/main/java/com/crud/tasks/domain/TrelloAttachmentsByType.java
@@ -2,24 +2,16 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class TrelloAttachmentsByType {
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("trello")
+    private Trello trello;
 }
